e2e6d8beca15eca07bdcc95962af630a53535aae
田中龍之介 <ry-tanaka@sios.com>
2024-01-18 20:12:55 +0900

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,51 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:tailwindcss/recommended",
+    "import",
+    "unused-imports",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "tailwindcss"],
+  rules: {
+    "unused-imports/no-unused-imports": "error",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin", // 組み込みモジュール
+          "external", // npmでインストールした外部ライブラリ
+          "internal", // 自作モジュール
+          ["parent", "sibling"],
+          "object",
+          "type",
+          "index",
+        ],
+        "newlines-between": "always", // グループ毎にで改行を入れる
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc", // 昇順にソート
+          caseInsensitive: true, // 小文字大文字を区別する
+        },
+        pathGroups: [
+          // 指定した順番にソートされる
+          // {
+          //   pattern: "@/components/common",  // パターン
+          //   group: "internal",               // グループ名
+          //   position: "before",              // どの位置に挿入するか
+          // },
+        ],
+      },
+    ],
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="text-2xl text-blue-500"> TEST</div>
+    </>
+  );
+}
+
+export default App;
