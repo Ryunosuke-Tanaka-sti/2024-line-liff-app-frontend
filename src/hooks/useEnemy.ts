c7fb0d20f3b6ee2b33b2a28c2e29b72869ce9405
田中龍之介 <ry-tanaka@sios.com>
2024-09-09 13:36:26 +0900

    swr

--- src/hooks/useEnemy.ts
+++ src/hooks/useEnemy.ts
@@ -2,6 +2,10 @@ import { fetchEnemy } from "src/api/Battle/api";
 import useSWR from "swr";
 
 export const useEnemy = () => {
-  const { data, error, isLoading, mutate } = useSWR("enemy", fetchEnemy);
+  const { data, error, isLoading, mutate } = useSWR("enemy", fetchEnemy, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
   return { data, error, isLoading, mutate };
 };
